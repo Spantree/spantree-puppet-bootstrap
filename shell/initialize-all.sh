6a30018d0636f7c4366a9d12bf1ff9c134c9a2a6
Cedric Hurst <cedric@spantree.net>
2014-11-25 12:05:41 -0600

    fixing script root issue

--- shell/initialize-all.sh
+++ shell/initialize-all.sh
@@ -1,6 +1,7 @@
 SCRIPT_ROOT=$(echo "$1")
+PROJECT_ROOT=$(echo "$2")
 
-$SCRIPT_ROOT/os-detect-setup.sh $SCRIPT_ROOT
-$SCRIPT_ROOT/initial-setup.sh $SCRIPT_ROOT
-$SCRIPT_ROOT/update-puppet.sh $SCRIPT_ROOT
-$SCRIPT_ROOT/librarian-puppet-vagrant.sh $SCRIPT_ROOT
+$SCRIPT_ROOT/os-detect-setup.sh $SCRIPT_ROOT $PROJECT_ROOT
+$SCRIPT_ROOT/initial-setup.sh $SCRIPT_ROOT $PROJECT_ROOT
+$SCRIPT_ROOT/update-puppet.sh $SCRIPT_ROOT $PROJECT_ROOT
+$SCRIPT_ROOT/librarian-puppet-vagrant.sh $SCRIPT_ROOT $PROJECT_ROOT
