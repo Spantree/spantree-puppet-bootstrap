35ac1c893badbc6a4d520869d43532f0148e763c
Sebastian Otaegui (black) <feniix@gmail.com>
2015-01-30 17:05:26 -0600

    fix vbguest and add simplified packer file for ovf

--- packer/install-vboxguest.sh
+++ packer/install-vboxguest.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
-. /tmp/common.sh
-set -x
-if [ ! -e /home/vagrant/.vbox_version ] ; then
-    exit 0
-fi
 
-# VirtualBox Additions
+# Without libdbus virtualbox would not start automatically after compile
+apt-get -y install --no-install-recommends libdbus-1-3
 
-# Installing the virtualbox guest additions
+# The netboot installs the VirtualBox support (old) so we have to remove it
+/etc/init.d/virtualbox-ose-guest-utils stop
+rmmod vboxguest
+aptitude -y purge virtualbox-ose-guest-x11 virtualbox-ose-guest-dkms virtualbox-ose-guest-utils
+aptitude -y install dkms
+
+# Install the VirtualBox guest additions
 VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
-VBOX_ISO=/home/vagrant/VBoxGuestAdditions_${VBOX_VERSION}.iso
-cd /tmp
-
-if [ ! -f $VBOX_ISO ] ; then
-    wget -q http://download.virtualbox.org/virtualbox/${VBOX_VERSION}/VBoxGuestAdditions_${VBOX_VERSION}.iso \
-        -O $VBOX_ISO
-fi
+VBOX_ISO=VBoxGuestAdditions_$VBOX_VERSION.iso
 mount -o loop $VBOX_ISO /mnt
-sh /mnt/VBoxLinuxAdditions.run
+yes|sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
 
+/etc/init.d/vboxadd setup
+
+#Cleanup VirtualBox
 rm $VBOX_ISO
