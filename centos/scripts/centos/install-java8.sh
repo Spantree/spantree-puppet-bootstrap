09a414e6d282bb03f7ba0fe8e6513b626bb0943d
Sebastian Otaegui (black) <feniix@gmail.com>
2015-05-05 19:55:30 -0500

    add vagrant box for centos

---
+++ centos/scripts/centos/install-java8.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -eux
+
+pushd /tmp
+
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u45-b14/jdk-8u45-linux-x64.rpm" -O jdk-8u45-linux-x64.rpm
+
+rpm -ivh jdk-8u45-linux-x64.rpm
+
+(
+cat <<'EOF'
+#!/usr/bin/env bash
+
+export JAVA_HOME=/usr/java/jdk1.8.0_45
+
+export PATH="${JAVA_HOME}/bin:${PATH}"
+
+EOF
+) > /etc/profile.d/java.sh
+
+chmod a+x /etc/profile.d/java.sh
+
+rm -rf jdk-8u45-linux-x64.rpm
+
+popd
